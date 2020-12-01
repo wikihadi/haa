ee31aeb4dfdc4510042d95f35529543452919ae1
Hadi Rafiee <H.Rafiee@Hamiran.local>
2020-12-01 17:10:17 +0330

    Initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+        mounted() {
+           var dataToSend = {
+	"method" : "login",
+	"input_type" : "JSON",
+	"response_type" : "JSON"
+};
+var restData = {
+	user_auth : {
+		user_name : "DemoUser",
+		password : "b57014dd3a80639cc4390294a0218401"
+	},
+	application : "TEST",
+	name_value_list : {
+		"name" : "language",
+		"value" : "fa_IR"
+	},
+	"meta_data":true,
+};
+
+
+        }
+    }
+</script>
---
+++ resources/views/certificate.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header text-right">
+                <!-- {{ __('Certificates') }} -->
+                    <a href="/certificate/create" class="btn btn-light">جدید</a>
+                
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif 
+
+                     <!-- {{ __('You are logged in!') }} -->
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                     <lab></lab>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
